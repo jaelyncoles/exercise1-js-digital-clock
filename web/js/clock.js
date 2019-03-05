4cb1e70c78ea96e7783d13992a0fd97472403ed5
jaelyn coles <jaelyncoles@Matthews-MBP-2.localdomain>
2019-03-05 15:30:17 -0500

    finished clock

---
+++ web/js/clock.js
@@ -0,0 +1,18 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var datestr=today.toLocaleTimeString("en-us");
+    document.getElementById('txt').innerHTML =
+    datestr;
+    var t = setTimeout(startTime, 500);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  
+ 
